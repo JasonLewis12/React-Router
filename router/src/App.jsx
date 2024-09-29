41a06e50953463416ac95e01f2866676e80b2f62
JasonLewis12 <Lewisjason64@yahoo.com>
2024-09-29 19:51:16 -0400

    added routes to blue, red amd home

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import Red from "./Red";
+import Blue from "./Blue";
+import Home from "./home";
 import "./App.css";
 
 function App() {
@@ -9,11 +12,18 @@ function App() {
     <>
       <div id="container">
         <h1>Hello React Router!</h1>
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to="/home">Home</Link>
+          <br />
+          <Link to="/blue">Blue</Link>
+          <br />
+          <Link to="/red">Red</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/home" element={<Home />} />
           </Routes>
         </div>
       </div>
